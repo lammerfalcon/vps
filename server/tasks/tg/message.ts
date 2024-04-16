e53e9383b9e810561aea43c602109c37a6651c0f
rodiongavrilov <rodiongavrilov@gmail.com>
2024-04-16 14:37:44 +0200

    test github actions

--- server/tasks/tg/message.ts
+++ server/tasks/tg/message.ts
@@ -1,12 +1,11 @@
 import {bot} from "~/server/plugins/tg-bot";
-
+let count = 0;
 export default defineTask({
     meta: {
         name: "tg:message",
         description: "send message to telegram bot",
     },
     async run({ payload, context }) {
-        let count = 1;
        await bot.api.sendMessage(952881284, `hello from nitro ${count} times` ).then((res) => {
            count++;
        })
