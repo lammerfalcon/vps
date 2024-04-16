c48e1fc808fc8953c12ecb506424cb7bfdeb7a22
rodiongavrilov <rodiongavrilov@gmail.com>
2024-04-16 13:19:36 +0200

    test github actions

--- server/api/test.ts
+++ server/api/test.ts
@@ -2,7 +2,7 @@ export default defineEventHandler(async (event) => {
     return {
         statusCode: 200,
         body: {
-            message: 'github actions',
+            message: 'change',
         },
     };
 })
