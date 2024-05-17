bf2b575533336c53acc45db7106e8624af524e66
rodiongavrilov <rodion.gavrilov@adskill.io>
2024-05-17 23:34:35 +0300

    test github actions

--- server/tasks/tg/message.ts
+++ server/tasks/tg/message.ts
@@ -1,4 +1,4 @@
-import {bot} from "~/server/plugins/tg-bot";
+// import {bot} from "~/server/plugins/tg-bot";
 let count = 0;
 export default defineTask({
     meta: {
@@ -6,9 +6,9 @@ export default defineTask({
         description: "send message to telegram bot",
     },
     async run({ payload, context }) {
-       await bot.api.sendMessage(952881284, `hello from nitro ${count} times` ).then((res) => {
-           count++;
-       })
+       // await bot.api.sendMessage(952881284, `hello from nitro ${count} times` ).then((res) => {
+       //     count++;
+       // })
 
         return { result: "Success" };
     },
